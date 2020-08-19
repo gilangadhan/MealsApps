08283ee15e673618f3053b80a500444be7a50cf4
arisupriatna14 <arisupriatna703@gmail.com>
2020-08-19 13:07:02 +0700

    add CustomIcon and Color extension

---
+++ MealsApps/Core/Common/Extensions/Color+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Ext.swift
+//  MealsApps
+//
+//  Created by Ari Supriatna on 19/08/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+import SwiftUI
+
+extension Color {
+    static var random: Color {
+        return Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
+    }
+}
---
+++ MealsApps/Core/Common/View/CustomIcon.swift
@@ -0,0 +1,21 @@
+//
+//  CustomIcon.swift
+//  MealsApps
+//
+//  Created by Ari Supriatna on 19/08/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomIcon()
+    }
+}
