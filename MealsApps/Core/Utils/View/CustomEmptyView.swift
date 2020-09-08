78120e2d9385985dd09db1ce48366d57aab1fa0b
arisupriatna14 <arisupriatna703@gmail.com>
2020-09-08 18:00:13 +0700

    add new assets

---
+++ MealsApps/Core/Utils/View/CustomEmptyView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomEmptyView.swift
+//  MealsApps
+//
+//  Created by Ari Supriatna on 08/09/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomEmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomEmptyView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomEmptyView()
+    }
+}
